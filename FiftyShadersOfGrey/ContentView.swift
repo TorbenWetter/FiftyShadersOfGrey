b5515b1ebfe0a5a1492fc4ce913956a2eb4efa02
Torben Wetter <git@torben.id>
2023-06-14 19:05:46 +0200

    Initial Commit

---
+++ FiftyShadersOfGrey/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FiftyShadersOfGrey
+//
+//  Created by Torben Wetter on 14.06.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FiftyShadersOfGrey/FiftyShadersOfGreyApp.swift
@@ -0,0 +1,17 @@
+//
+//  FiftyShadersOfGreyApp.swift
+//  FiftyShadersOfGrey
+//
+//  Created by Torben Wetter on 14.06.23.
+//
+
+import SwiftUI
+
+@main
+struct FiftyShadersOfGreyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
